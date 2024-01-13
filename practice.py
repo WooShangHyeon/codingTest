e57e357d17258e6f59daff8f48cd8bb87a1edb53
wooshanghyeon <woosh9912@naver.com>
2024-01-13 21:58:51 +0900

    hash 베스트엘범 수정

--- practice.py
+++ practice.py
@@ -1,7 +1,6 @@
 def solution(genres, plays):
     answer = []
     cnt = 0
-    cnt = 2
     a = {}
     b = {}
     c = {}
@@ -22,11 +21,17 @@ def solution(genres, plays):
             a[genre] = numOfPlay
             b[genre] = i
 
-    while a and cnt > 0:
+    while a and cnt < 2:
+        cnt += 1
         x = max(a, key=a.get)
         a.pop(x)
         answer.append(b[x])
         if x in c:
             answer.append(c[x])
-        
+ 
     return answer
+
+a=["classic", "pop", "classic", "classic", "pop","kor","kor"]
+b=[500, 600, 150, 800, 2500,1000,300]
+print(solution(a,b))
+# 문제에서는 2개의 장르에 대한 것만 출력하면되는것 같은데 막상 테스트케이스 정답 유무를 보면 모든 장르에 대한 2개의 곡을 출력해야하는것 같다...
